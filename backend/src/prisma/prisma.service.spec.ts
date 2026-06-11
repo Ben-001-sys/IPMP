2eebec6c1fd080e35158c29fdf7e915753b68bce
Emmanuel Owusu Boakye <boakyeowusu738@gmail.com>
2026-06-11 16:05:45 +0000

    gggt

--- backend/src/prisma/prisma.service.spec.ts
+++ backend/src/prisma/prisma.service.spec.ts
@@ -9,7 +9,7 @@ describe('PrismaService', () => {
       providers: [PrismaService],
     }).compile();
 
-    service = module.get<PrismaService>(PrismaService);
+    service =  module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
